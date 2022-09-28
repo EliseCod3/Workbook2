5a13cff76fefa2b55abc3dfac36b106e25eb84da
elise bell <eliseraynahasawsomesauce@gmail.com>
2022-09-28 09:07:35 -0400

    Finished calculating the net worth

---
+++ Exercises/MathScripts/net_worth.js
@@ -0,0 +1,9 @@
+//sample inputs
+var assets = 200000.96;
+var debts = 1983.62;
+
+//calculations
+var totalNetWorth = assets - debts;
+
+//outputs
+console.log("Your net worth is " + totalNetWorth);
