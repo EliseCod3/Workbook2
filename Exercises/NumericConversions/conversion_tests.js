830c7ba27471f11d8d71f28030bfb5543494a6ec
elise bell <eliseraynahasawsomesauce@gmail.com>
2022-09-29 09:17:22 -0400

    Began exercise

---
+++ Exercises/NumericConversions/conversion_tests.js
@@ -0,0 +1,12 @@
+// Description: This script tests various numeric
+// conversion techniques
+// Author: Rayna B. Newprogrammer
+
+var a = " 101.1 ";
+var b = "55";
+var c = "402 Stevens";
+var d = "Number 5 ";
+
+console.log("The result is " + parseInt(c));
+console.log("The result is " + parseFloat(c));
+console.log("The result is " + Number(c));
