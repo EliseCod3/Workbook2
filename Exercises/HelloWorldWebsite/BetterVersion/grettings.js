68c0e551f25c5024fd53326e3b0118c958180672
elise bell <eliseraynahasawsomesauce@gmail.com>
2022-09-29 14:01:17 -0400

    Added a few more exercises

---
+++ Exercises/HelloWorldWebsite/BetterVersion/grettings.js
@@ -0,0 +1,10 @@
+"use strict";
+
+function showGreeting() {
+    const messsageDiv = document.getElementById("messageDiv");
+    messageDiv.innerHTML = "<h1>Hello World!</h1>";
+    
+}
+
+showGretting();
+
---
+++ Exercises/PayRate/pay_rate.js
@@ -0,0 +1,22 @@
+"use strict";
+
+function calculatePay() {
+    //query html elements on the page and put them into variables
+    const hoursWorkedInput = document.getElementById("hoursWorkedInput");
+    const payRateInput  = document.getElementById("payRateInput");
+    const resultParagraph = document.getElementById("resultParagraph");
+    
+    //calculate something
+    let hoursWorked = Number(hoursWorkedInput.value);
+    let payRate = Number(payRateInput.value);
+    let pay = hoursWorked * payRate;
+    let message = `Pay is ${pay}`;
+    
+    //create your message to display
+    console.log(hoursWorked * payRate);
+    
+    //display message
+    resultParagraph.innerText = message;
+}
+
+calculatePay();
